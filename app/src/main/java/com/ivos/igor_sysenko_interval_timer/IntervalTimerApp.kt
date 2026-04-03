1040501085a4a07214a2312680da1d7463bbb9ea
Igor Sysenko <isysenko@yandex.ru>
2026-04-03 21:38:29 +0300

    Initial commit + base config

---
+++ app/src/main/java/com/ivos/igor_sysenko_interval_timer/IntervalTimerApp.kt
@@ -0,0 +1,8 @@
+package com.ivos.igor_sysenko_interval_timer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class IntervalTimerApp : Application() {
+}
