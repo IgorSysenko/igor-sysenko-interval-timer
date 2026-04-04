c0ffdd21cccac5b3e2d8b6069f1f7d7cd9b36cd6
Igor Sysenko <isysenko@yandex.ru>
2026-04-04 17:42:38 +0300

    ktor configured

---
+++ data/src/main/java/com/ivos/data/di/KtorModule.kt
@@ -0,0 +1,54 @@
+package com.ivos.data.di
+
+import com.ivos.data.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.http.HttpHeaders
+import io.ktor.http.headers
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+private const val defaultTimeout = 30L
+
+@[Module InstallIn(SingletonComponent::class)]
+object KtorModule {
+    @[Provides Singleton]
+    fun provideHttpClient(): HttpClient {
+        return HttpClient(OkHttp) {
+
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+
+            defaultRequest {
+                url(BuildConfig.BASE_URL)
+                headers {
+                    append("App-Token", BuildConfig.APP_TOKEN)
+                    append(HttpHeaders.Authorization, "Bearer ${BuildConfig.AUTH_TOKEN}")
+                    append(HttpHeaders.Accept, "application/json")
+                }
+            }
+
+            expectSuccess = true
+
+            engine {
+                config {
+                    connectTimeout(defaultTimeout, TimeUnit.SECONDS)
+                    readTimeout(defaultTimeout, TimeUnit.SECONDS)
+                }
+            }
+        }
+    }
+}
